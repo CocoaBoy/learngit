938340938a5c8ba0172e3a168e24ba6dee6bf070
superior_yong <1375867505@qq.com>
2018-01-24 10:25:30 +0800

    Update AppDelegate.m

--- learngitDemo/learngitDemo/AppDelegate.m
+++ learngitDemo/learngitDemo/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     //This is Override point
+    // add update
     return YES;
 }
 
